b192944cb685d0f645cd3673cbf52323ce097b2f
Arber Sefa <arber.sefa@utb.ecutbildning.se>
2022-06-02 20:49:02 +0200

    Added loading-spinner when entering the site

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import BuyAndSell from "./components/BuyAndSell";
 import StockCounter from "./components/StockCounter";
@@ -6,6 +6,8 @@ import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import createTheme from "@mui/material/styles/createTheme";
 import green from "@mui/material/colors/green";
 import purple from "@mui/material/colors/purple";
+
+import PacmanLoader from "react-spinners/PacmanLoader";
 
 const theme = createTheme({
   palette: {
@@ -19,7 +21,20 @@ const theme = createTheme({
 });
 
 function App() {
-  return (
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  return loading ? (
+    <div className="loading">
+      <PacmanLoader size={30} color={"#3f99f9"} loading={loading} />
+    </div>
+  ) : (
     <ThemeProvider theme={theme}>
       <div className="site-wrapper">
         <div className="inner-wrapper">
